7cc2fdac36ce3f5cd190b250f5ae9a0fb629c8f9
Sarthak Joshi <iamsarthakjoshi@gmail.com>
2019-11-10 11:56:26 +1100

    folder name changes routes to router | some minor changes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 
-import routes from 'routes'
+import routes from 'router'
 import logger from 'common/logger'
 
 const app = express()
